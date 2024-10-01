3be887bb808beaf25aa7b868b32389ec6daa7e55
Pedro Jorge <pedrojorgedesouzac@gmail.com>
2024-10-01 12:52:29 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from back import tabela_cruzada
-df = pd.read_csv('d:/PROJETOS/PI A1/vgsales_com_clusters.csv')
+df = pd.read_csv('vgsales_com_clusters.csv')
 
 st.title("Tabela Cruzada")
 st.write(tabela_cruzada(df, 'Platform', 'Genre'))
