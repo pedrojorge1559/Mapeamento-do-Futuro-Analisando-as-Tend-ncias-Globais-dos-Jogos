d44835cb1faf5d07e0e91d4eb72dcfd1b8f52b85
Pedro Jorge <pedrojorgedesouzac@gmail.com>
2024-10-01 12:55:30 -0300

    Add files via upload

--- back.py
+++ back.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-df = pd.read_csv('d:/PROJETOS/PI A1/vgsales_com_clusters.csv')
+df = pd.read_csv('vgsales_com_clusters.csv')
 
 def tabela_cruzada(df, linhas, colunas):
     tabela_cruzada = pd.crosstab(df[linhas], df[colunas])
